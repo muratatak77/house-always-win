79bce0f010b7f409fef546277535cdfc95f5167f
murat atak <muratatak@murats-MacBook-Pro-2.local>
2025-08-29 19:30:45 -0600

    Remove default test folder. Use Rpec only

---
+++ config/initializers/generators.rb
@@ -0,0 +1,14 @@
+Rails.application.config.generators do |g|
+  g.test_framework :rspec
+
+  # do not create extra spec types if you want clean repo
+  g.helper_specs false
+  g.view_specs   false
+  g.routing_specs false
+  g.controller_specs false
+  g.system_tests nil   # no system tests (minitest)
+
+  # optional: do not create helper/assets when generate
+  g.helper false
+  g.assets false
+end
